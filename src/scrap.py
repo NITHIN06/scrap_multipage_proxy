d3c2f8ba23ac35f45428de5acad8152c8a651ade
NITHIN06 <anithin1234@gmail.com>
2023-06-18 15:35:46 +0530

    added proxy and yt channel

--- src/scrap.py
+++ src/scrap.py
@@ -1,34 +1,65 @@
 from bs4 import BeautifulSoup
 import requests
 import pandas as pd
+import json
 
+
+api_key = "API_KEY"
 HEADERS = ({'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36',
-            'Accept-Language':'en-US, en;q=0.5'})
+            'Accept-Language':'en-US, en;q=0.5', 
+            "Authorization": f"Bearer {api_key}"
+            })
+
+
+# HEADERS = {"Authorization": f"Bearer {api_key}"}
+
 
 def get_content(url):
     print(url)
-    site = requests.get(url,  headers=HEADERS)
-    soup = BeautifulSoup(site.text, "html.parser")
+    site = requests.get(url, headers=HEADERS)
+    # site = client.get(url)
+    soup = BeautifulSoup(site.content, "html.parser")
 
     links = soup.find_all("a", {"jscontroller":"M9mgyc"}, class_=False)
     names = soup.select('h3.LC20lb')
+    # print(soup)
+
+    links = [link['href'] for link in links]
+    link = ''
+    if link=='':
+        for i in links:
+            if 'channel' in i or f"youtube.com/{txt.split()[-1]}" in i:
+                link = i
+    print(link)
 
     next_url = soup.find('a', {"id":"pnnext"})
-    return (pd.DataFrame({"names": [name.text for name in names], "links": [link['href'] for link in links]}), next_url)
+    return ({"names": [name.text for name in names], "links": links}, next_url, link)
 
-df = pd.DataFrame(columns=['names', 'links'])
+df = {"names": [], "links": [], "channel": ''}
 
-txt = input()
+# txt = "youtube.com openinapp.co"
+txt = "youtube openinapp"
 url = 'https://google.com/search?q=' + txt
 pre_link = 'https://google.com/'
 print(url)
 
 next_url = 'none'
-while df.shape[0]<100 or next_url:
-    curr_df, next_url = get_content(url)
-    df = pd.concat([df, curr_df], axis=0)
+link = ''
+while len(df['links'])<10000 or next_url:
+    curr_df, next_url, link = get_content(url)
+    df['links'].extend(curr_df['links'])
+    df['names'].extend(curr_df['names'])
     if next_url:
         url = pre_link+next_url['href']
-    print(df.shape[0])
+    # print(df.shape[0])
 
-print(df.head())
+    break
+df['channel'] = link
+print(df)
+df['links'] = df['links'][:10000]
+df['names'] = df['names'][:10000]
+json_object = json.dumps(df, indent=4)
+ 
+# Writing to sample.json
+with open("../records.json", "w") as outfile:
+    outfile.write(json_object)
